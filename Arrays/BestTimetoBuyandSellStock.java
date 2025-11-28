38480aa18d83ff31f8eadf4ec14c1141f7bc6beb
Rohit Shaw <rohitshaw0110@gmail.com>
2025-11-28 17:15:45 +0530

    Create BestTimetoBuyandSellStock.java

---
+++ Arrays/BestTimetoBuyandSellStock.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public int maxProfit(int[] prices) 
+    {
+        if (prices == null || prices.length == 0) return 0;
+        
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int price : prices)
+        {
+            if(price < minPrice)
+            {
+                minPrice = price;
+            }
+            else 
+            {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+}
